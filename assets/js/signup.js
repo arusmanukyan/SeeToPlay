96e7cf433c156d914baa50dd2f5c455f3219abc2
arusmanukyan <manukyan.arus@gmail.com>
2016-11-15 19:04:27 -0800

    sign up

---
+++ assets/js/signup.js
@@ -0,0 +1,34 @@
+  $(document).ready(function(){
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCjB_F2kuSDsezSfXwQMTA5hXRC6kD87v4",
+    authDomain: "classproject1-6a90e.firebaseapp.com",
+    databaseURL: "https://classproject1-6a90e.firebaseio.com",
+    storageBucket: "classproject1-6a90e.appspot.com",
+    messagingSenderId: "306797326634"
+  };
+  firebase.initializeApp(config);
+
+  	var database = firebase.database();
+
+    $("#signUp").on("click",function(){
+
+    	var userName = $("#contact-name").val().trim();
+        var userEmail = $("#email").val().trim();
+
+        database.ref().push({
+        	userName: userName,
+        	userEmail: userEmail,
+
+        })
+
+        $("#contact-name").val("");
+        $("#email").val("");
+
+
+    });
+
+
+  });
